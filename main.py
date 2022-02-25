c9f99680b2c1b30b27d567b5299d131cdc5d3f8d
Cat Burroughs <44904570+catburroughs@users.noreply.github.com>
2022-02-25 10:36:31 +0000

    main set_soundlist added

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 npath = NPath()
 
 npath.set_volume()
-npath.make_soundlist()
+#npath.make_soundlist()
+npath.set_soundlist()
+npath.print_soundlist()
 npath.print_volume()
 npath.play_board()
