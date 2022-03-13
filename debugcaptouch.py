2d0414df59bef55a9da917b0cb2cd467e7e2d160
catburroughs <cat.burroughs@yahoo.com>
2022-03-13 12:06:27 +0000

    "working on board"

--- debugcaptouch.py
+++ debugcaptouch.py
@@ -1,13 +1,27 @@
 import time
+import sys
 import board
 import busio
-import adafruit_mpr121
 import pygame
+import Adafruit_MPR121.MPR121 as MPR121
 
-i2c = busio.I2C(board.SCL, board.SDA)
-mpr121 = adafruit_mpr121.MPR121(i2c)
+
+print('Adafruit MPR121 Capacitive Touch Sensor Test')
+
+# Create MPR121 instance.
+cap = MPR121.MPR121()
+
+# Initialize communication with MPR121 using default I2C bus of device, and
+# default I2C address (0x5A).  On BeagleBone Black will default to I2C bus 0.
+if not cap.begin():
+    print('Error initializing MPR121.  Check your wiring!')
+    sys.exit(1)
+#i2c = busio.I2C(board.SCL, board.SDA)
+
 
 pygame.init()
+#cap.begin()
+cap.set_thresholds(6, 6)
 
 clap1 = pygame.mixer.Sound('samples/1 clap-analog.wav')
 hat1 = pygame.mixer.Sound('samples/1 openhat-acoustic01.wav')
@@ -28,13 +42,32 @@ for x in soundList:
     x.set_volume(.65)
 
 
+#while True:
+ #   for i in range(len(soundList)):
+#        if cap[i].value:
+#            soundList[i].play()
+ #           print("Input {} touched!".format(i))
+
+
+print('Press Ctrl-C to quit.')
+last_touched = cap.touched()
 while True:
-    for i in range(len(soundList)):
-        if mpr121[i].value:
-            soundList[i].play()
-            print("Input {} touched!".format(i))
-
-
-
+    current_touched = cap.touched()
+    # Check each pin's last and current state to see if it was pressed or released.
+    for i in range(12):
+        # Each pin is represented by a bit in the touched value.  A value of 1
+        # means the pin is being touched, and 0 means it is not being touched.
+        pin_bit = 1 << i
+        # First check if transitioned from not touched to touched.
+        if current_touched & pin_bit and not last_touched & pin_bit:
+            print('{0} touched!'.format(i))
+            if (soundList[i]):
+                soundList[i].play()
+        # Next check if transitioned from touched to not touched.
+        if not current_touched & pin_bit and last_touched & pin_bit:
+            print('{0} released!'.format(i))
+    # Update last state and wait a short period before repeating.
+    last_touched = current_touched
+    time.sleep(0.1)
 
 #print("Hello World")
